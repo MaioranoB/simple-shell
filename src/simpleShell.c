1af6f20c9a93c7551f846bb79d5101dd40bb6706
Bernardo Maiorano <54644125+MaioranoB@users.noreply.github.com>
2020-11-04 00:41:38 -0300

    using fgets

--- src/simpleShell.c
+++ src/simpleShell.c
@@ -6,13 +6,14 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#define MAX_SIZE 50
+#define MAX_SIZE 25
 int SIGNAL_FLAG = 0;
 int COMMAND_RUNNING = 0;
 
 void sig_handler(int signo){
     if (signo == SIGUSR1 && !COMMAND_RUNNING){
-		printf("\n\nreceived SIGUSR1\n");
+		printf("\n\nReceived SIGUSR1\n");
+		printf("Press enter to restart the shell\n");
 		SIGNAL_FLAG =  1;
 	}
 }
@@ -20,29 +21,40 @@ void sig_handler(int signo){
 int main(void){
 	signal(SIGUSR1, sig_handler);
 
-	char *argumentos[MAX_SIZE]; //Array de argumentos que sera passado pra função execvp
-	char comando[MAX_SIZE]; //Variável que vai receber o comando
 	int num_args; //Variável que vai receber o numero de argumentos
-	char arg[MAX_SIZE]; //Variável que vai receber cada argumento
+	char user_input[MAX_SIZE]; //Variável que vai receber as entradas do usuário
+	char *argumentos[MAX_SIZE+2]; //Array de argumentos que sera passado pra função execvp
+	//Exemplo: para o comando ping 8.8.8.8 -c 5
+	//Devemos passar: execvp("ping",["ping","8.8.8.8","-c","5",NULL]);
 
 	while(1){
 
 		//PASSOS 1 e 2
 		if (SIGNAL_FLAG == 0){
 			printf("Qual comando você quer executar? ");
-			scanf("%s",comando);
-			argumentos[0] = strdup(comando);
+			fgets(user_input, MAX_SIZE, stdin);
+    		for (int i=0; i<=MAX_SIZE; i++){
+        		if (user_input[i] == '\n'){
+            		user_input[i] = '\0';
+        		}
+    		}
+			argumentos[0] = strdup(user_input);
 		}
 
 		//PASSOS 3 e 4
 		if (SIGNAL_FLAG == 0){
 			printf("Quantos argumentos você quer digitar? ");
-			if (scanf("%d",&num_args) == 0){
+			fgets(user_input,MAX_SIZE,stdin);
+			num_args = atoi(user_input);
+			
+			//Verificar se o usuário passou uma entrada invalida pro número de args
+			if (num_args == 0 && user_input[0] != '0' && SIGNAL_FLAG == 0){
 				printf("Invalid input\n");
 				exit(EXIT_FAILURE);
 			}
-			printf("\n");
+
 			argumentos[num_args+1] = NULL;
+			printf("\n");
 		}
 
 		//PASSO 5
@@ -51,15 +63,20 @@ int main(void){
 				break;
 			}
 
-			printf("Digite o argumento %d: ", i);
-			scanf("%s",arg);
-			argumentos[i] = strdup(arg);
+			printf("Digite o argumento %d: ",i);
+			fgets(user_input, MAX_SIZE, stdin);
+			for (int j=0; j<=MAX_SIZE; j++){
+        		if (user_input[j] == '\n'){
+            		user_input[j] = '\0';
+        		}
+			}
+			argumentos[i] = strdup(user_input);
 		}
 
 		if (SIGNAL_FLAG == 1){
 			SIGNAL_FLAG = 0;
 		}
-		//Se chegar cheagar aqui sem a flag do sinal, deve sair do while infinito e executar o comando
+		//Se chegar aqui sem a flag do sinal, deve sair do while infinito e executar o comando
 		else{
 			break;
 		}
@@ -73,8 +90,8 @@ int main(void){
 		//Run command
 		execvp(argumentos[0],argumentos);
 		
-		//O execvp retorna se não encontrar o comando
-		printf("Command %s not found!\n",argumentos[0]); 
+		//O execvp só retorna se não encontrar o comando
+		printf("\nCommand not found: %s\n",argumentos[0]); 
 		exit(EXIT_FAILURE); 
 	}
 	else {
